b881df67b9c749f2ee02fce7fb7e80c8e8bc0a63
Herbert Lago <hebertpdl@gmail.com>
2023-02-15 20:49:37 -0300

    typing alert component

--- src/01-js-to-ts/alert.tsx
+++ src/01-js-to-ts/alert.tsx
@@ -1,11 +1,17 @@
 import { useIntl } from 'react-intl';
 
-export function Alert({ messageId, variant }) {
+type Props = {
+  messageId: string;
+  variant: 'primary' | 'secondary' | 'success' | 'danger' | 'warning' | 'info' | 'light' | 'dark';
+};
+
+export const Alert: React.FC<Props> = ({ messageId, variant }) => {
   const { formatMessage } = useIntl();
 
   if (!messageId) {
     throw new Error('The messageId prop is required');
   }
+
   return (
     <div className={`alert alert-${variant}`} role="alert">
       {formatMessage({ id: messageId })}
